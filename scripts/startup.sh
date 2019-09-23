0705c7adcc47fde58848987fc3b3c4d334822cc2
lawyerance <lawyerance@gmail.com>
2019-09-23 21:05:38 +0800

    [MOD] modify start script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 
 SOURCE_PATH=$(cd `dirname $BASH_SOURCE`;pwd)
-export PROJRCT_BASE_PATH=${SOURCE_PATH%/*}
+export PROJECT_BASE_PATH=${SOURCE_PATH%/*}
 
-echo "The path of the program is: $PROJRCT_BASE_PATH"
+echo "The path of the program is: $PROJECT_BASE_PATH"
 
-cd $PROJRCT_BASE_PATH
+cd $PROJECT_BASE_PATH
+
 java -jar -Djava.ext.dirs=./lib spring-boot-example.jar
